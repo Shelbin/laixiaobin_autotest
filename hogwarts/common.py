51bed6e9b417eff1bf6327b2a488a2262aaf1a0f
Shelbin <399394409@qq.com>
2021-09-16 11:57:29 +0800

    common工具优化

--- hogwarts/common.py
+++ hogwarts/common.py
@@ -1,4 +1,5 @@
 # 定义一个common的类，它的父类是object
+import json
 import requests
 
 class Common(object):
@@ -15,13 +16,13 @@ class Common(object):
         res = requests.get(url)
         return res
 
-    def post(self, uri, params=''):
+    def post(self, uri, params='', headers=''):
         # 拼凑访问地址
         url = self.url_root + uri
         if len(params) > 0:
             # 如果有参数，那么通过post方式访问对应的url，并将参数赋值给requests.post默认参数data
             # 返回request的Response结果，类型为requests的Response类型
-            res = requests.post(url, data=params)
+            res = requests.post(url, data=json.dumps(params), headers=headers)
         else:
             # 如果无参数，访问方式如下
             # 返回request的Response结果，类型为requests的Response类型
